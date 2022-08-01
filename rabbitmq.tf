e10492e804ec134d999a4b8640b7f78ee2a43c8c
Raghu K <raghuk.vit@gmail.com>
2022-08-01 06:33:48 +0530

    Update terraform/tf-module-rabbitmq/

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -1,11 +1,19 @@
 resource "aws_spot_instance_request" "instance" {
-  ami                  = data.aws_ami.ami.image_id
-  spot_price           = data.aws_ec2_spot_price.spot_price.spot_price
-  instance_type        = var.INSTANCE_TYPE
-  wait_for_fulfillment = true
+  ami                    = data.aws_ami.ami.image_id
+  spot_price             = data.aws_ec2_spot_price.spot_price.spot_price
+  instance_type          = var.INSTANCE_TYPE
+  wait_for_fulfillment   = true
+  subnet_id              = var.PRIVATE_SUBNET_IDS[0]
+  vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
     Name = local.TAG_PREFIX
   }
+}
+
+resource "aws_ec2_tag" "name-tag" {
+  resource_id = aws_spot_instance_request.instance.spot_instance_id
+  key         = "Name"
+  value       = local.TAG_PREFIX
 }
 
