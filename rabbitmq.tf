5367aa945c06526001917df5ca51b89b98dd9abc
Raghu K <raghuk.vit@gmail.com>
2022-08-01 06:30:17 +0530

    Update terraform/tf-module-rabbitmq/

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -1,6 +1,6 @@
 resource "aws_spot_instance_request" "instance" {
   ami                  = data.aws_ami.ami.image_id
-  spot_price           = data.aws_ec2_spot_price.spot_price
+  spot_price           = data.aws_ec2_spot_price.spot_price.spot_price
   instance_type        = var.INSTANCE_TYPE
   wait_for_fulfillment = true
 
