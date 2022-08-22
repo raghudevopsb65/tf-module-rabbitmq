4b6203cedfd0d3df51e58518b9dae4eeca170e80
Raghu K <raghuk.vit@gmail.com>
2022-08-22 05:51:07 +0530

    Update terraform/tf-module-rabbitmq/

--- data.tf
+++ data.tf
@@ -6,11 +6,17 @@ data "aws_secretsmanager_secret_version" "secret" {
   secret_id = data.aws_secretsmanager_secret.secret.id
 }
 
+//data "aws_ami" "ami" {
+//  //executable_users = ["self"]
+//  most_recent = true
+//  name_regex  = "base-with-ansible"
+//  owners      = ["self"]
+//}
+
 data "aws_ami" "ami" {
-  //executable_users = ["self"]
   most_recent = true
-  name_regex  = "base-with-ansible"
-  owners      = ["self"]
+  name_regex  = "Centos-7-DevOps-Practice"
+  owners      = ["973714476881"]
 }
 
 data "aws_ec2_spot_price" "spot_price" {
